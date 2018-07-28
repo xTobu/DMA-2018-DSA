f31eeec780dafbf01852af45e140ec7f363ca470
xTobu <junxiang.oriint@gmail.com>
2018-07-28 13:29:41 +0800

    create sponsors

---
+++ pages/sponsors.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="wrapper">
+        <div class="head head_sponsors">
+      <div class="tittle"><span>贊助單位</span><span class="sub">SPONSORS</span></div>
+    </div>
+    <main class="sponsors">
+      <div class="sponsors-content">
+        <h5>主辦單位</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>創新夥伴</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>創新夥伴</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>奇點夥伴</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>鑽石級贊助</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>白金級贊助</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>金級贊助</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>協辦單位</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>執行單位</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+        <h5>網路空間贊助</h5>
+        <div class="logo-content">
+          <ul>
+            <li><img src="~assets/img/logo/logo-01-01.jpg"></li>
+          </ul>
+        </div>
+      </div>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+	asyncData({ redirect }) {
+	},
+	head() {
+		return {
+			title: '贊助單位',
+		};
+	},
+	layout: 'layoutIndex',
+	data() {
+		return {};
+	},
+	methods: {},
+	created() {
+		// console.log('created');
+	},
+
+	mounted() {},
+};
+</script>
+
+<style scoped>
+@import '~/assets/css/sponsors.css';
+</style>
+
