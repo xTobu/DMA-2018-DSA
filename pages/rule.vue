04b116d3f75929bae88c1e27131965798fd3edc7
xTobu <junxiang.oriint@gmail.com>
2018-07-28 13:13:46 +0800

    create rule

---
+++ pages/rule.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="wrapper">
+        <div class="head head_rule">
+      <div class="tittle"><span>活動辦法</span><span class="sub">TERMS AND CONDITIONS</span></div>
+    </div>
+    <main class="rule">
+      <div class="rule-content">
+        <h5>2018數位奇點獎活動時程</h5>
+        <div class="grey-bg">
+          <div class="timeline">
+            <div class="circle-date">
+              <div class="date">AUG/02</div>
+              <div class="text">北京徵件<br>說明會</div>
+            </div>
+            <div class="circle-date">
+              <div class="date">AUG/15</div>
+              <div class="text">台北徵件<br>說明會</div>
+            </div>
+            <div class="circle-date circle-green">
+              <div class="date">AUG/20<br>|<br>SEP/20</div>
+              <div class="text">徵件期</div>
+            </div>
+            <div class="circle-date circle-green">
+              <div class="date">SEP/05<br>|<br>SEP/06</div>
+              <div class="text">奇點論壇 <sup>1</sup></div>
+            </div>
+            <div class="circle-date circle-green">
+              <div class="date">OCT/08<br>|<br>OCT/17</div>
+              <div class="text">初審</div>
+            </div>
+            <div class="circle-date circle-green">
+              <div class="date">OCT/29<br>|<br>NOV/07</div>
+              <div class="text">複審</div>
+            </div>
+            <div class="circle-date">
+              <div class="date">NOV/14</div>
+              <div class="text">決審會議</div>
+            </div>
+            <div class="circle-date circle-red">
+              <div class="date">NOV/16</div>
+              <div class="text wide">DigiAsia x DSA論壇 <sup>2</sup><br>數位奇點獎頒獎典禮 <sup>3</sup></div>
+            </div>
+          </div>
+        </div>
+        <p><sup>註1</sup> 全天：華南銀行國際會議廳<span></span><span><sup>註2</sup> 白天：花博公園爭艷館</span><span><sup>註3</sup> 晚上：艾麗酒店 </span></p>
+      </div>
+      <div class="rule-content">
+        <h5>參賽資格、報名方式、獎項內容、規格要求</h5>
+        <h6>參賽資格</h6>
+        <p>2017年9月1日起至2018年8月31日止，刊載於世界各地之數位媒體與裝置，包含數位科技運用，均可由台灣政府核准立案之廣告代理公司、數位科技公司或含數位相關、製作公司與廣告主報名參加。</p>
+        <h6>報名方式</h6>
+        <p>請於2018年9月20日報名截止前，於數位奇點獎官網註冊登錄：</p>
+        <ul>
+          <li>所有參賽作品提交方式皆為網路線上提交。</li>
+          <li>請於官網註冊報名後，即可上傳報名作品，並請同時完成繳費動作。</li>
+          <li>同一件作品，可報名不同的獎項。</li>
+          <li>請注意，同一件獎項內，報名作品相關之廣告公司（製作公司）、廣告主、媒體，及其他有關單位請擇一代表，勿重複提交。</li>
+          <li>每件作品限由一個單位報名，並需事先取得相關單位及版權單位之同意。</li>
+          <li>參加本獎項且得獎之作品，下一屆將不得再報名。</li>
+          <li>報名表填寫不完整、作品規格或參賽資格不符，以致影響評審結果，恕不另行通知。</li>
+        </ul>
+        <h6>獎項內容</h6>
+        <p>一、綜合類獎項：<br>數位創意類、創新應用技術類、整合行銷類、媒體應用類：由各類項中各選出金獎、銀獎、銅獎，頒予獎座乙座。 <br>二、年度獎項</p>
+        <ul>
+          <li>年度創新技術獎：從創新應用技術類各項金銀銅獎作品中，選出一名，頒予獎座乙座。</li>
+          <li>全場大獎：從四大類各項金獎中，選出一名，頒予獎座。</li>
+        </ul>
+        <h6>參賽作品規格要求</h6>
+        <ul>
+          <li>所有作品必須上傳報名網站的方式參賽。</li>
+          <li>作品均可同時報名一至多個獎項，不受類別與項目別限制。</li>
+          <li>若作品為其他語言，請提供中文說明。</li>
+          <li>所有作品都必須以PDF的方式呈現與說明，內容需包含：目標、策略、創意、執行、結果或成效等說明。</li>
+        </ul>
+        <h6>作品輔助之規格需求</h6>
+        <ul>
+          <li>影片類—請上傳YouTube後，提供連結即可。</li>
+          <li>平面類—請提供JPG檔案，大小不超過30MB。</li>
+          <li>不接受TIF檔案。</li>
+          <li>其餘規定請參照參賽辦法之說明。</li>
+          <li>獎項賽事內容若有更動，請以官網公告為準。</li>
+        </ul>
+        <h6>報名費用</h6>
+        <p>會員NTD$3,500元/件/項，非會員NTD$5,000元/件/項。單一報名單位報名超過十件，再予以八折優惠。</p>
+        <h6>繳件方式</h6>
+        <p>本屆參賽作品繳交一律使用線上上傳，經由官方網站註冊登入後，將作品資料上傳即可。</p>
+        <h6>付款方式</h6>
+        <p>收到報名確認信後，將款項匯入指定帳戶。</p>
+      </div>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+	asyncData({ redirect }) {
+		
+	},
+	head() {
+		return {
+			title: '活動辦法',
+		};
+	},
+	layout: 'layoutIndex',
+	data() {
+		return {};
+	},
+	methods: {},
+	created() {
+		// console.log('created');
+	},
+
+	mounted() {},
+};
+</script>
+
+<style scoped>
+@import '~/assets/css/rule.css';
+</style>
+
