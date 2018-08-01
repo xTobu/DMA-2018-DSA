323e79516eff64097c3b5226f326b048a4b59664
Junxiang <junxiang.oriint@gmail.com>
2018-08-01 14:23:11 +0800

    wip

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import qs from 'qs';
-export default function({ app, route, redirect }) {
-	return app.$axios({
-		method: 'POST',
-		data: qs.stringify({
-			act_mode: 'mydata',
-		}),
-		url: '/user.ashx',
-	})
+export default function({ app, redirect }) {
+	return app
+		.$axios({
+			method: 'POST',
+			data: qs.stringify({
+				act_mode: 'mydata',
+			}),
+			url: '/user.ashx',
+		})
 		.then(response => {
 			// console.log(response);
 		})
