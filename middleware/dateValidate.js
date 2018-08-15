6b134f2484c5750a1d7c46495ea536b0e28992e4
Junxiang <junxiang.oriint@gmail.com>
2018-08-15 21:02:18 +0800

    wip

--- middleware/dateValidate.js
+++ middleware/dateValidate.js
@@ -1,7 +1,7 @@
 
 export default function({ app, redirect ,store}) {
     //console.log(store.state.user)
-    if (new Date() > new Date('2018-08-15T00:00:00')) {
+    if (new Date() > new Date('2018-09-20T23:59:59')) {
           
         redirect('/u/list');
     }
