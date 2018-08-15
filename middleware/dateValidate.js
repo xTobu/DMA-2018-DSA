d496c028adfb517caff95eaccc97f8d1a3a67cdc
Junxiang <junxiang.oriint@gmail.com>
2018-08-15 21:02:24 +0800

    wip

--- middleware/dateValidate.js
+++ middleware/dateValidate.js
@@ -6,10 +6,6 @@ export default function({ app, redirect ,store}) {
         redirect('/u/list');
     }
 	if (process.client) {
-         // this.$swal({
-        //     type: 'error',
-        //     title: '對不起，活動時間已經結束',
-        // });     
         alert('對不起，活動時間已經結束')
         
     }
